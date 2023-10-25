2768fd3f5ca867ae403889e0f2cd7144ff1b81d8
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2023-10-25 20:34:46 +0530

    Update primaryConstructor.kt

--- primaryConstructor.kt
+++ primaryConstructor.kt
@@ -1,8 +1,12 @@
 //Implementing primary constructor
 
 class Car(name: String, model: Int){
+    var name: String? = null
+    var model: Int? = null
+    
     init {
-        println("my car is ${name} and it is model ${model}")
+        this.name = name
+        this.model = model
     }
 }
 
@@ -10,4 +14,9 @@ fun main() {
 	var myFirstCar = Car("Mercedes", 2020)
     var mySecondCar = Car("BMW", 2019)
     var myThirdCar = Car("Ford", 2016)
+    
+    println("My first car is ${myFirstCar.name} and it is ${myFirstCar.model} model")
+    println("My second car is ${mySecondCar.name} and it is ${mySecondCar.model} model")
+    println("My third car is ${myThirdCar.name} and it is ${myThirdCar.model} model")
+
 }
