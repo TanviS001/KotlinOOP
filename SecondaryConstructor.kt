cec37c441803ebde3af7a68b8f37192b934bcc4f
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2023-10-25 20:06:19 +0530

    Create SecondaryConstructor.kt

---
+++ SecondaryConstructor.kt
@@ -0,0 +1,23 @@
+//Implementing secondary constructor
+
+open class Car{
+    var name: String? = null
+    var model: Int? = null
+    
+    constructor(name: String, model: Int){
+        this.name = name
+        this.model = model
+        
+    }
+}
+
+fun main() {
+    var myFirstCar = Car("Mercedes" , 2020)
+    var mySecondCar = Car("Ford", 2019)
+    var myThirdCar = Car("Lamborghini", 2015)
+    
+    println("My first car is ${myFirstCar.name} and it is ${myFirstCar.model} model")
+    println("My second car is ${mySecondCar.name} and it is ${mySecondCar.model} model")
+    println("My third car is ${myThirdCar.name} and it is ${myThirdCar.model} model")
+
+}
