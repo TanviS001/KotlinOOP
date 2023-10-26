552b8cdf5ec2d8ae87d78ad8b3b8682a1aa25516
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2023-10-26 11:30:17 +0530

    Create MultipleInheritance.kt

---
+++ MultipleInheritance.kt
@@ -0,0 +1,49 @@
+// Super Class
+open class Vehicle{
+    
+    fun start(){
+        println("Vehicle has started")
+    }
+    
+    open fun run(){
+        println("Vehicle is running at speed 40km/hr")
+    }
+    
+    fun stop(){
+        println("Vehicle has stopped")
+    }
+    
+}
+
+//Sub Class
+open class Car: Vehicle(){
+    
+    open override fun run(){
+        println("Car is running at speed 70km/hr")
+    }
+    
+}
+class Mercedes: Vehicle(){
+    
+    override fun run(){
+        println("Mercedes is running at speed 223km/hr")
+    }
+    
+}
+
+
+fun main() {
+    
+    var myCar = Car()
+    var myMercedes = Mercedes()
+    
+    myCar.start()
+    myCar.run()
+    myCar.stop()
+
+    println("---------------------------------------")
+    
+    myMercedes.start()
+    myMercedes.run()
+    myMercedes.stop()
+}
